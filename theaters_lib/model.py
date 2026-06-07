2fc6a39fcec7c0cd3af132dc698725353cf08d5f
gaziles <eliza.gajda@student.wat.edu.pl>
2026-06-07 18:43:05 +0200

    feat: prepare data structures for theaters clients and employees

---
+++ theaters_lib/model.py
@@ -0,0 +1,128 @@
+theaters: list = [
+
+
+    {
+        'name': 'Teatr Muzyczny Roma',
+        'location': 'Warszawa',
+        'performances': ['Hamilton', 'Cats'],
+        'clients': ['Anna', 'Kamil'],
+        'employees': ['Jan Kowalski', 'Maria Nowak']
+    },
+
+    {
+        'name': 'Teatr Narodowy',
+        'location': 'Warszawa',
+        'performances': ['Dziady', 'Wesele'],
+        'clients': ['Piotr', 'Magda'],
+        'employees': ['Adam Nowak', 'Karolina Lis']
+    },
+
+    {
+        'name': 'Teatr Komedia',
+        'location': 'Warszawa',
+        'performances': ['Mayday', 'Boeing Boeing'],
+        'clients': ['Tomasz', 'Ewa'],
+        'employees': ['Paweł Wójcik', 'Agnieszka Mazur']
+    },
+
+
+    {
+        'name': 'Teatr Bagatela',
+        'location': 'Kraków',
+        'performances': ['Hamlet', 'Makbet'],
+        'clients': ['Ola', 'Paweł'],
+        'employees': ['Piotr Zieliński', 'Ewa Lis']
+    },
+
+    {
+        'name': 'Teatr Ludowy',
+        'location': 'Kraków',
+        'performances': ['Zemsta', 'Balladyna'],
+        'clients': ['Monika', 'Kacper'],
+        'employees': ['Tadeusz Król', 'Joanna Kania']
+    },
+
+    {
+        'name': 'Teatr STU',
+        'location': 'Kraków',
+        'performances': ['Biesy', 'Król Lear'],
+        'clients': ['Natalia', 'Michał'],
+        'employees': ['Rafał Sikora', 'Anna Duda']
+    },
+
+
+    {
+        'name': 'Teatr Polski',
+        'location': 'Wrocław',
+        'performances': ['Makbet', 'Wesele'],
+        'clients': ['Karolina', 'Tomek'],
+        'employees': ['Adam Wiśniewski', 'Natalia Kaczmarek']
+    },
+
+    {
+        'name': 'Teatr Capitol',
+        'location': 'Wrocław',
+        'performances': ['Mistrz i Małgorzata', 'Hair'],
+        'clients': ['Patryk', 'Julia'],
+        'employees': ['Szymon Krawczyk', 'Ewa Baran']
+    },
+
+    {
+        'name': 'Teatr Lalek',
+        'location': 'Wrocław',
+        'performances': ['Pinokio', 'Kopciuszek'],
+        'clients': ['Kinga', 'Marcin'],
+        'employees': ['Jan Urban', 'Alicja Wrona']
+    },
+
+
+    {
+        'name': 'Teatr Wybrzeże',
+        'location': 'Gdańsk',
+        'performances': ['Hamlet', 'Zbrodnia i kara'],
+        'clients': ['Robert', 'Zuzanna'],
+        'employees': ['Mateusz Pawlak', 'Monika Sowa']
+    },
+
+    {
+        'name': 'Teatr Miniatura',
+        'location': 'Gdańsk',
+        'performances': ['Alicja w Krainie Czarów', 'Piotruś Pan'],
+        'clients': ['Marta', 'Bartek'],
+        'employees': ['Andrzej Bąk', 'Katarzyna Lis']
+    },
+
+    {
+        'name': 'Teatr Szekspirowski',
+        'location': 'Gdańsk',
+        'performances': ['Romeo i Julia', 'Otello'],
+        'clients': ['Damian', 'Sandra'],
+        'employees': ['Tomasz Kurek', 'Iwona Maj']
+    },
+
+
+    {
+        'name': 'Teatr Powszechny',
+        'location': 'Radom',
+        'performances': ['Szalone nożyczki', 'Koziołek Matołek'],
+        'clients': ['Radek', 'Ilona'],
+        'employees': ['Jan Motyka', 'Szymon Kędzierski']
+    },
+
+    {
+        'name': 'Teatr Kameralny',
+        'location': 'Radom',
+        'performances': ['Skąpiec', 'Moralność Pani Dulskiej'],
+        'clients': ['Marek', 'Joanna'],
+        'employees': ['Rafał Czarnecki', 'Anna Borkowska']
+    },
+
+    {
+        'name': 'Teatr Miejski',
+        'location': 'Radom',
+        'performances': ['Antygona', 'Zemsta'],
+        'clients': ['Łukasz', 'Weronika'],
+        'employees': ['Kamil Górski', 'Marta Sikora']
+    }
+
+]
